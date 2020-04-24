08845b56b0daf259f42a0cd308c4b39835b69059
Samarpan <samarpanchakravarty308@gmail.com>
2020-04-24 13:12:33 +0530

    Completed styling of chat page

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -4,23 +4,60 @@ document.addEventListener('DOMContentLoaded', () => {
     let room="General";
     joinRoom("General");
 
-    socket.on('message', data => {
-        const p = document.createElement('p');
-        const br = document.createElement('br');
-        const span_uname = document.createElement('span');
-        const span_time = document.createElement('span');
 
-        if(data.username) {
-            span_uname.innerHTML = data.username;
-            span_time.innerHTML = data.time_stamp; 
-            p.innerHTML = span_uname.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_time.outerHTML;
-            document.querySelector('#message-section').append(p);
+    socket.on('message', data => {
+
+        // Display current message
+        if (data.msg) {
+            const p = document.createElement('p');
+            const span_username = document.createElement('span');
+            const span_timestamp = document.createElement('span');
+            const br = document.createElement('br')
+            // Display user's own message
+            if (data.username == username) {
+                    p.setAttribute("class", "my-msg");
+
+                    // Username
+                    span_username.setAttribute("class", "my-username");
+                    span_username.innerText = data.username;
+
+                    // Timestamp
+                    span_timestamp.setAttribute("class", "timestamp");
+                    span_timestamp.innerText = data.time_stamp;
+
+                    // HTML to append
+                    p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML
+
+                    //Append
+                    document.querySelector('#message-section').append(p);
+            }
+            // Display other users' messages
+            else if (typeof data.username !== 'undefined') {
+                p.setAttribute("class", "others-msg");
+
+                // Username
+                span_username.setAttribute("class", "other-username");
+                span_username.innerText = data.username;
+
+                // Timestamp
+                span_timestamp.setAttribute("class", "timestamp");
+                span_timestamp.innerText = data.time_stamp;
+
+                // HTML to append
+                p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
+
+                //Append
+                document.querySelector('#message-section').append(p);
+            }
+            // Display system message
+            else {
+                printSystemMessage(data.msg);
+            }
+
+
         }
-        else {
-            printSystemMessage(data.msg);
-        }
-        
-    })
+        scrollDownChatWindow();
+    });
 
     document.querySelector('#send-message').onclick = () => {
         socket.send({'msg': document.querySelector('#user-message').value, 'username': username, 'room': room });
@@ -42,20 +79,43 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    document.querySelector('#logout-btn').onclick = () => {
+        leaveRoom(room);
+    }
+
     function leaveRoom(room) {
         socket.emit('leave', {'username': username, 'room': room});
+
+        document.querySelectorAll('.select-room').forEach(p => {
+            p.style.removeProperty("background-color");
+            p.style.color="#3e71d6";
+        });
     }
 
     function joinRoom(room){
         socket.emit('join', {'username': username, 'room': room});
 
+        document.querySelector('#' + CSS.escape(room)).style.color = "#f0f1f5";
+        document.querySelector('#' + CSS.escape(room)).style.backgroundColor = "#081540";
+
         document.querySelector('#message-section').innerHTML = '';
         document.querySelector('#user-message').focus();
     }
 
+    function scrollDownChatWindow() {
+        const chatWindow = document.querySelector("#message-section");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+
+
     function printSystemMessage(msg) {
         const p = document.createElement('p');
+        p.setAttribute("class", "system-msg");
         p.innerHTML = msg;
         document.querySelector('#message-section').append(p);
+        scrollDownChatWindow();
+
+        // Autofocus on text box
+        document.querySelector("#user-message").focus();
     }
 })
